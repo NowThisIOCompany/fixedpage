ab23d93c0e712df2b9e53ce7b12b40c94ac603a3
NowThisIO <denis@nowthis.io>
2018-10-13 11:24:56 -0700

    intially

---
+++ output/index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>NowThis.io</title>
+        <meta charset="UTF-8">
+        <link rel="icon" href="/favicon.ico" type="image/x-icon" />
+    </head>
+    <style>
+        html{
+            width:100%; 
+            height:100%; 
+            background:url("/nowthis.io.png") center center no-repeat;
+        }
+    </style>
+</html>
